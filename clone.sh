cc7aa866d7418db2b9758a61927b0cc2dbc33e2f
Timothy Krell <timothykrell@gmail.com>
2020-06-22 09:33:38 -0400

    Set up tmux plugin manager

--- clone.sh
+++ clone.sh
@@ -4,5 +4,8 @@ echo "Cloning repositories..."
 
 CODE=$HOME/Code
 
+# For tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Personal
 # git clone git@github.com:driesvints/cashier-paddle.git $CODE/cashier-paddle
